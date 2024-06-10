4797c4e3b637e6565efa6e521ff8b40d53ef94f0
Yewon Lee <yewlee0218@gmail.com>
2024-06-10 14:04:24 -0400

    Revert "tried editing further"
    This reverts commit 70f5e6e1ae88336c97a0b4dc0e3dc9ad70fa4732.

--- player.java
+++ player.java
@@ -27,19 +27,19 @@ public class player extends Actor
             if (w.couple()){
                 w.slapWin(w.playerCard, "couple");
             if (w.marriage()){
-                w.slapWin(w.playerCard, "Marriage");
+                w.slapWin(w.playerCard, "marriage");
                 w.ifPatternSlapped = true; 
             } 
             else if (w.divorce()){
-                w.slapWin(w.playerCard, "Divorce");
+                w.slapWin(w.playerCard, "divorce");
                 w.ifPatternSlapped = true; 
             }
             else if (w.couple()){
-                w.slapWin(w.playerCard, "Double");
+                w.slapWin(w.playerCard, "couple");
                 w.ifPatternSlapped = true; 
             }
             else if (w.sandwich()){
-                w.slapWin(w.playerCard, "Sandwich");
+                w.slapWin(w.playerCard, "sandwich");
                 w.ifPatternSlapped = true; 
             }
             else if (w.marriage()){
@@ -51,11 +51,11 @@ public class player extends Actor
                 w.ifPatternSlapped = true; 
             }
             else if (w.redTen()){
-                w.slapWin(w.playerCard, "Red Ten");
+                w.slapWin(w.playerCard, "redTen");
                 w.ifPatternSlapped = true; 
             }
             else if (w.topBottom()){
-                w.slapWin(w.playerCard, "Top & Bottom");
+                w.slapWin(w.playerCard, "topBottom");
                 w.ifPatternSlapped = true; 
             }
             else if (w.threeInARow()){
